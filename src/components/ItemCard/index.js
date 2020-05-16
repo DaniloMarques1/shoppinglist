dfd8d8fa8fe6e590769f5d3f817721c010929fb2
Danilo Marques <danilomarques20@hotmail.com>
2020-05-16 11:28:19 -0300

    Adicionar evento do clique no botao de editar no itemCard

--- src/components/ItemCard/index.js
+++ src/components/ItemCard/index.js
@@ -4,14 +4,14 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import {colors} from '../../utils/colors'
 import {Container, ItemView, ItemText, IconView, IconButton} from './styles';
 
-function ItemCard({itemName}) {
+function ItemCard({itemName, onPress}) {
   return (
     <Container>
       <ItemView>
         <ItemText>{itemName}</ItemText>
       </ItemView>
       <IconView>
-        <IconButton>
+        <IconButton onPress={onPress}>
           <Icon name="edit" size={17} color={colors.primaryBlue} />
         </IconButton>
       </IconView>
