818d835311bb7b15b70080b8d7dca68f1e9567f8
Danilo Marques <danilomarques20@hotmail.com>
2020-05-16 15:36:30 -0300

    Adicionando formatador de currency no helper

---
+++ src/utils/helper.js
@@ -0,0 +1,12 @@
+export default class Helper {
+  /**
+   * Format the string(money) based on the type provided 
+   * @number value - ex: 42.32
+   * @string type - ex: pt-BR, en-EN
+   */
+  static formatCurrency(value, type) {
+    //TODO: lazy solution because javascript sucks
+    const roundedValue = value.toFixed(2);
+    return `R$ ${roundedValue}`;
+  }
+}
