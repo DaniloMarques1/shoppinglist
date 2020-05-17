b24261e40dee425c6163cb77d17abd7df7118ec9
Danilo Marques <danilomarques20@hotmail.com>
2020-05-16 22:28:27 -0300

    Criando um componente picker para selecionar a cateogira do item

---
+++ src/components/Select/index.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+import {Picker} from '@react-native-community/picker';
+import Category from '../../utils/category';
+
+function Select({selectedValue, onChange}) {
+  return (
+    <>
+      <Picker 
+        selectedValue={selectedValue}
+        onValueChange={(itemValue) => onChange(itemValue)}
+      >
+        <Picker.Item label={Category.BEEF} value="beef" />
+        <Picker.Item label={Category.CLEANING} value="cleaning" />
+        <Picker.Item label={Category.ALIMENT} value="aliment" />
+        <Picker.Item label={Category.DRINK} value="drink" />
+        <Picker.Item label={Category.FROZEN} value="frozen" />
+        <Picker.Item label={Category.DESSERT} value="dessert" />
+        <Picker.Item label={Category.FLAVORING} value="flavoring" />
+      </Picker>
+    </>
+  );
+}
+
+export default Select;
