f580b80e68279f2d1f92e6fdcc08e3b17b34770f
Danilo Marques <danilomarques20@hotmail.com>
2020-05-22 17:54:53 -0300

    Ajustando pagina de update para validações de input

--- src/pages/UpdateItem/index.js
+++ src/pages/UpdateItem/index.js
@@ -18,6 +18,8 @@ import Input from '../../components/Input';
 import {colors} from '../../utils/colors';
 import {Alert} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
+import * as yup from 'yup';
+import {Formik} from 'formik';
 
 function UpdateItem({navigation, route}) {
   const dispatch = useDispatch();
@@ -66,64 +68,79 @@ function UpdateItem({navigation, route}) {
       }
     ]);
   }
+  
+  const validationSchema = yup.object().shape({
+    itemName: yup.string().required("Por favor dê um nome para o item"),
+    itemQtd: yup.number().typeError("Número").required("Obrigatorio"),
+    category: yup.string().required("selecione uma categoria")
+  });
 
   return (
     <Container>
       <Title> Informe os dados do novo item</Title>
-      <Label>Digite o nome do produto: </Label>
-      <Input
-        placeholder="Ex: café"
-        length={40}
-        type="default"
-        value={itemName}
-        onChangeText={setItemName}
-      />
-      <InputLine>
-        <InputPrice>
-          <Label>Digite o preço: </Label>
-          <Input
-            placeholder="Ex: 5.50"
-            length={40}
-            type="numeric"
-            value={itemPrice}
-            onChangeText={setItemPrice}
-          />
-        </InputPrice>
-        <InputQtd>
-          <Label>Quantidade: </Label>
-          <Input
-            placeholder="Ex: 2"
-            length={40}
-            type="numeric"
-            value={itemQtd}
-            onChangeText={setItemQtd}
-          />
-        </InputQtd>
-      </InputLine>
-      <Label>Seleciona a categoria </Label>
-      <CheckBoxContainer>
-        <CheckBox
-          value={purchased}
-          onValueChange={setPurchased}
-         />
-        <Label>Comprado</Label>
-      </CheckBoxContainer>
-      <ButtonContainer>
-        <Button 
-          onPress={handleUpdateItem}
-          color={colors.primaryBlue}
-          icon="edit"
-          text="Atualizar"
+      <Formik
+        initialValues={{
+          itemName: item.name,
+          itemPrice: item.price,
+          itemQtd: item.qtd,
+          category: route.params?.category || ''
+        }}
+      >
+        <Label>Digite o nome do produto: </Label>
+        <Input
+          placeholder="Ex: café"
+          length={40}
+          type="default"
+          value={itemName}
+          onChangeText={setItemName}
         />
-      </ButtonContainer>
-      <ButtonContainer>
-        <Button 
-          onPress={handleOpenAlert}
-          color={colors.primaryRed}
-          icon="delete"
-          text="Apagar"
-        />
-      </ButtonContainer>
+        <InputLine>
+          <InputPrice>
+            <Label>Digite o preço: </Label>
+            <Input
+              placeholder="Ex: 5.50"
+              length={40}
+              type="numeric"
+              value={itemPrice}
+              onChangeText={setItemPrice}
+            />
+          </InputPrice>
+          <InputQtd>
+            <Label>Quantidade: </Label>
+            <Input
+              placeholder="Ex: 2"
+              length={40}
+              type="numeric"
+              value={itemQtd}
+              onChangeText={setItemQtd}
+            />
+          </InputQtd>
+        </InputLine>
+        <Label>Seleciona a categoria </Label>
+        <CheckBoxContainer>
+          <CheckBox
+            value={purchased}
+            onValueChange={setPurchased}
+           />
+          <Label>Comprado</Label>
+        </CheckBoxContainer>
+        <ButtonContainer>
+          <Button 
+            onPress={handleUpdateItem}
+            color={colors.primaryBlue}
+            icon="edit"
+            text="Atualizar"
+          />
+        </ButtonContainer>
+        <ButtonContainer>
+          <Button 
+            onPress={handleOpenAlert}
+            color={colors.primaryRed}
+            icon="delete"
+            text="Apagar"
+          />
+        </ButtonContainer>
+      </Formik>
     </Container>
   );
 }
